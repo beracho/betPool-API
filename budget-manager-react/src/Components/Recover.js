744f843d2a62a17b6ec5ec59cd27f814d48d5611
Adrian Berazain <marcelo.berazain@gmail.com>
2024-02-13 19:31:31 +0800

    Adds validations to fields

--- budget-manager-react/src/Components/Recover.js
+++ budget-manager-react/src/Components/Recover.js
@@ -1,9 +1,22 @@
 import "../Styles/SigninPage.css"
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import validator from 'validator';
 
 const Recover = () => {
-    const [email, setEmail] = useState();
+    const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState('');
+
+    const handleEmailChange = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value);
+
+        if (validator.isEmail(e.target.value)) {
+            setEmailError('')
+        } else {
+            setEmailError('Enter valid Email!')
+        }
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -19,8 +32,9 @@ const Recover = () => {
                     type="text"
                     required
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={(e) => handleEmailChange(e)}
                 />
+                <span style={{ color: 'red' }}>{emailError}</span>
             </div>
             <div className="login-link">
                 <Link to="/login">Back to login</Link>
--- budget-manager-react/src/Components/Register.js
+++ budget-manager-react/src/Components/Register.js
@@ -3,17 +3,45 @@ import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import validator from 'validator';
 
 const Register = () => {
-    const [username, setUsername] = useState();
-    const [password, setPassword] = useState();
-    const [email, setEmail] = useState();
-    const [telephone, setTelephone] = useState();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [telephone, setTelephone] = useState("");
+
     const [isPending, setIsPending] = useState(false);
     const [data, setData] = useState(null);
     const [error, setError] = useState(false);
 
     const navigate = useNavigate();
+
+    const [emailError, setEmailError] = useState('');
+    const [telephoneError, setTelephoneError] = useState('');
+
+    const handleEmailChange = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value);
+
+        if (validator.isEmail(e.target.value)) {
+            setEmailError('')
+        } else {
+            setEmailError('Enter valid Email!')
+        }
+    }
+
+    const handlePhoneChange = (e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+        setTelephone(e.target.value);
+
+        if (validator.isMobilePhone(e.target.value)) {
+            setTelephoneError('')
+        } else {
+            setTelephoneError('Enter valid phone number!')
+        }
+    }
 
     const notifyError = (erroMessage) => toast.error(erroMessage, {
         transition: Bounce,
@@ -33,49 +61,67 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setIsPending(true);
-        const registerObject = {
-            username: username,
-            password: password,
-            email: email,
-            telephone: telephone
-        };
 
-        fetch('https://localhost:7152/api/Auth/Register', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(registerObject)
-        }).then(res => {
-            if (!res.ok) {
-                return res.text().then(err => {
-                    throw Error(err);
-                });
-            }
-            return res.json()
-        }).then(data => {
-            setData(data)
-            setIsPending(false);
-            notifySuccess("Account registered successfully!")
+        var fieldsValidated = true;
 
-            navigate('/');
-        }).catch(err => {
-            setIsPending(false);
-            if (isJSON(err.message)) {
-                setError(err.message);
-                var errObject = JSON.parse(err.message);
-                if (errObject.errors) {
-                    setError(errObject.errors);
-                    for (const [key, value] of Object.entries(errObject.errors)) {
-                        value.forEach(errorMessage => {
-                            notifyError(errorMessage);
-                        });
-                    }
+        if (validator.isEmail(email)) {
+            setEmailError('')
+        } else {
+            setEmailError('Enter valid Email!')
+            fieldsValidated = false;
+        }
+        if (validator.isMobilePhone(telephone)) {
+            setTelephoneError('')
+        } else {
+            setTelephoneError('Enter valid phone number!')
+            fieldsValidated = false;
+        }
+
+        if (fieldsValidated) {
+            const registerObject = {
+                username: username,
+                password: password,
+                email: email,
+                telephone: telephone
+            };
+
+            fetch('https://localhost:7152/api/Auth/Register', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(registerObject)
+            }).then(res => {
+                if (!res.ok) {
+                    return res.text().then(err => {
+                        throw Error(err);
+                    });
                 }
-            } else {
-                setError(err.message);
-                notifyError(err.message);
-            }
+                return res.json()
+            }).then(data => {
+                setData(data)
+                setIsPending(false);
+                notifySuccess("Account registered successfully!")
 
-        })
+                navigate('/');
+            }).catch(err => {
+                setIsPending(false);
+                if (isJSON(err.message)) {
+                    setError(err.message);
+                    var errObject = JSON.parse(err.message);
+                    if (errObject.errors) {
+                        setError(errObject.errors);
+                        for (const [key, value] of Object.entries(errObject.errors)) {
+                            value.forEach(errorMessage => {
+                                notifyError(errorMessage);
+                            });
+                        }
+                    }
+                } else {
+                    setError(err.message);
+                    notifyError(err.message);
+                }
+            })
+        }
+
     }
 
     return (
@@ -99,8 +145,9 @@ const Register = () => {
                     type="text"
                     required
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={(e) => handleEmailChange(e)}
                 />
+                <span style={{ color: 'red' }}>{emailError}</span>
             </div>
 
             <div className="form-group">
@@ -120,8 +167,9 @@ const Register = () => {
                     className="form-field"
                     type="text"
                     value={telephone}
-                    onChange={(e) => setTelephone(e.target.value)}
+                    onChange={(e) => handlePhoneChange(e)}
                 />
+                <span style={{ color: 'red' }}>{telephoneError}</span>
             </div>
 
             {!isPending && <button className="button">Register</button>}
